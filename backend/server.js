c47a2ffa765ce99beb3e37030e861a13e831da11
RJ Umair <umairajput711@gmail.com>
2025-05-23 22:14:09 +0500

    New Railway file added for backend

--- backend/server.js
+++ backend/server.js
@@ -386,3 +386,7 @@ app.get("/", (req, res) => {
 
 // ✅ Start Server
 const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`✅ Server listening on port ${PORT}`);
+});
